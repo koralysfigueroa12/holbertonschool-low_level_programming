a37ff05facd46d95f618bbb8f1af7577a6c87287
koralysfigueroa12 <koralys12@yahoo.com>
2025-10-17 09:42:00 -0400

    hola

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main - Prints string
  *
  * Return: 0
+ *
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-		
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
