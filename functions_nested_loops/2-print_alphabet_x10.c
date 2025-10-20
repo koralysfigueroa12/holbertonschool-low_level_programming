d791d0fadffce2743ce99892867d95759bc09674
koralysfigueroa12 <koralys12@yahoo.com>
2025-10-20 14:56:47 -0400

    hola

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10  - Prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char letter;
 
