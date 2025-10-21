6a0f310d9a96ef58514afe300b9c2c0abe444361
koralysfigueroa12 <koralys12@yahoo.com>
2025-10-21 11:36:19 -0400

    mn3

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,5 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char c);
+
 #endif /* MAIN_H */
 
