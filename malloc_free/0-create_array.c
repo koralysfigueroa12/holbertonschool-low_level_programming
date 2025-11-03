c68705924ec49f78fa68fd5d54d077376fbc34e1
Koralys Figueroa <koralys12@yahoo.com>
2025-11-03 11:36:43 -0400

    ya

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,8 +11,8 @@ char *create_array(unsigned int size, char c)
 {
     char *array;
     unsigned int i;
-
-    if (size == 0)
+  
+	if (size == 0)
         return (NULL);
 
     array = malloc(size * sizeof(char));
@@ -21,6 +21,5 @@ char *create_array(unsigned int size, char c)
 
     for (i = 0; i < size; i++)
         array[i] = c;
-
-    return (array);
+     return (array);
 }
