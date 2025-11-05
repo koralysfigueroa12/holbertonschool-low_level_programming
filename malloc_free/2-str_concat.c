bcb0a33b983d732173b52983aadeeafac1b5dd7f
Koralys Figueroa <koralys12@yahoo.com>
2025-11-05 10:01:53 -0400

    vamos haber

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,14 +7,14 @@
  * @s2: second string
  *
  * Return: pointer to a newly allocated space in memory which contains
- * the contents of s1, followed by the contents of s2, and null terminated
- * If NULL is passed, treat it as an empty string
- * Return NULL on failure
+ * the contents of s1, followed by the contents of s2, and null terminated.
+ * If NULL is passed, treat it as an empty string.
+ * Return NULL on failure.
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, len1 = 0, len2 = 0;
-	char *concat;
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *res;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -26,17 +26,16 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2] != '\0')
 		len2++;
 
-	concat = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (concat == NULL)
+	res = malloc((len1 + len2 + 1) * sizeof(char));
+	if (res == NULL)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
-
+		res[i] = s1[i];
 	for (j = 0; j < len2; j++)
-		concat[i + j] = s2[j];
+		res[i + j] = s2[j];
 
-	concat[i + j] = '\0';
+	res[i + j] = '\0';
 
-	return (concat);
+	return (res);
 }
