774b52f028fcf329ec36da816dc6be7d59b828bc
Koralys Figueroa <koralys12@yahoo.com>
2025-11-05 10:38:14 -0400

    vamos haber

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,36 +6,39 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: pointer to a newly allocated space in memory which contains
- * the contents of s1, followed by the contents of s2, and null terminated
- * If NULL is passed, treat it as an empty string.
- * NULL on failure.
+ * Return: pointer to a newly allocated space in memory
+ *         containing s1, followed by s2 and null terminated
+ *         NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = 0, len2 = 0, i, j;
 	char *concat;
+	int i, j, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	/* Calculate lengths of s1 and s2 */
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
-	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	/* Allocate memory for concatenated string + null terminator */
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
 
+	/* Copy s1 into concat */
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
+
+	/* Copy s2 after s1 */
 	for (j = 0; j < len2; j++)
 		concat[i + j] = s2[j];
 
 	concat[i + j] = '\0';
-
 	return (concat);
 }
