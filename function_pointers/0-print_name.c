503a1e8feb81290a4443146e09eea57da377dc45
Koralys Figueroa <koralys12@yahoo.com>
2025-11-10 10:30:31 -0400

    espero que este bien

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls a function to print a name
+ * @name: pointer to the name to print
+ * @f: pointer to function that prints a name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
