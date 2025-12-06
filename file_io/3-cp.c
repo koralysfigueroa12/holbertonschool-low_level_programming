92b195b4cddab8fc4a83dec303ddf356ced17b4c
Koralys Figueroa <koralys12@yahoo.com>
2025-12-06 13:16:30 -0500

    hola

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,60 +15,60 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;
-    ssize_t n_read, n_written;
-    char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
+	ssize_t n_read, n_written;
+	char buffer[BUFFER_SIZE];
 
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", argv[2]);
-        if (close(fd_from) == -1)
-            dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
-        exit(99);
-    }
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		if (close(fd_from) == -1)
+			dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		exit(99);
+	}
 
-    while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        n_written = write(fd_to, buffer, n_read);
-        if (n_written != n_read)
-        {
-            dprintf(2, "Error: Can't write to %s\n", argv[2]);
-            if (close(fd_from) == -1)
-                dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
-            if (close(fd_to) == -1)
-                dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
-            exit(99);
-        }
-    }
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written != n_read)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			if (close(fd_from) == -1)
+				dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+			if (close(fd_to) == -1)
+				dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+			exit(99);
+		}
+	}
 
-    if (n_read == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        if (close(fd_from) == -1)
-            dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
-        if (close(fd_to) == -1)
-            dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
-        exit(98);
-    }
+	if (n_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		if (close(fd_from) == -1)
+			dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		if (close(fd_to) == -1)
+			dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+		exit(98);
+	}
 
-    if (close(fd_from) == -1)
-        dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
-    if (close(fd_to) == -1)
-        dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 
-    return (0);
+	return (0);
 }
