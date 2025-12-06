4d4c4efcd4c26383dbf042105fbdeaeed7571d16
Koralys Figueroa <koralys12@yahoo.com>
2025-12-06 12:05:33 -0500

    hola

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,55 +2,54 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
- * @head: pointer to pointer to head of list
- * @index: index of the node to delete (starting at 0)
+ * delete_head - deletes the head node
+ * @head: pointer to pointer to head
  *
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: 1 if succeeded
+ */
+static int delete_head(dlistint_t **head)
+{
+    dlistint_t *tmp = *head;
+
+    *head = tmp->next;
+    if (*head)
+        (*head)->prev = NULL;
+
+    free(tmp);
+    return (1);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes node at index of a dlistint_t list
+ * @head: pointer to pointer to head of list
+ * @index: index of the node to delete
+ *
+ * Return: 1 if succeeded, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp;
-	unsigned int len = 0, i;
-	dlistint_t *cur;
+    dlistint_t *cur;
+    unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+    if (head == NULL || *head == NULL)
+        return (-1);
 
-	/* compute length */
-	cur = *head;
-	while (cur)
-	{
-		len++;
-		cur = cur->next;
-	}
+    if (index == 0)
+        return (delete_head(head));
 
-	/* index out of range */
-	if (index >= len)
-		return (-1);
+    cur = *head;
+    for (i = 0; cur && i < index; i++)
+        cur = cur->next;
 
-	/* delete head */
-	if (index == 0)
-	{
-		tmp = *head;
-		*head = tmp->next;
-		if (*head)
-			(*head)->prev = NULL;
-		free(tmp);
-		return (1);
-	}
+    if (cur == NULL)
+        return (-1);
 
-	/* find node at index */
-	cur = *head;
-	for (i = 0; i < index; i++)
-		cur = cur->next;
+    if (cur->prev)
+        cur->prev->next = cur->next;
 
-	/* relink neighbors */
-	if (cur->prev)
-		cur->prev->next = cur->next;
-	if (cur->next)
-		cur->next->prev = cur->prev;
+    if (cur->next)
+        cur->next->prev = cur->prev;
 
-	free(cur);
-	return (1);
+    free(cur);
+    return (1);
 }
